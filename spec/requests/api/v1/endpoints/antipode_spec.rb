bfc7465bf50e875dd055e36ab60af1d4942f3518
Matt Levy <milevy1@gmail.com>
2019-06-03 10:23:00 -0600

    Refactored antipode endpoint spec to test for every key

--- spec/requests/api/v1/endpoints/antipode_spec.rb
+++ spec/requests/api/v1/endpoints/antipode_spec.rb
@@ -9,23 +9,14 @@ describe 'Antipode API endpoint' do
 
     json_response = JSON.parse(response.body)
 
-    expect(json_response).to be_present
-
-    expected = {
-              	"data": [{
-              		"id": "1",
-              		"type": "antipode",
-              		"attributes": {
-              			"location_name": "Antipode City Name",
-              			"forecast": {
-              				"summary": "Mostly Cloudy",
-              				"current_temperature": "72"
-              			},
-              			"search_location": "Hong Kong"
-              		}
-              	}]
-              }
-
-    expect(json_response).to eq(expected)
+    expect(json_response['data']).to be_present
+    expect(json_response['data'][0]['id']).to be_present
+    expect(json_response['data'][0]['type']).to be_present
+    expect(json_response['data'][0]['attributes']).to be_present
+    expect(json_response['data'][0]['attributes']['location_name']).to be_present
+    expect(json_response['data'][0]['attributes']['forecast']).to be_present
+    expect(json_response['data'][0]['attributes']['forecast']['summary']).to be_present
+    expect(json_response['data'][0]['attributes']['forecast']['current_temperature']).to be_present
+    expect(json_response['data'][0]['attributes']['search_location']).to be_present
   end
 end
