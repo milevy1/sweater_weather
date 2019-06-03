2725c298cec94c46a071974ff228c43d0c523cb8
Matt Levy <milevy1@gmail.com>
2019-06-03 10:35:55 -0600

    Adds DarkSkyService spec for forecast_summary and forecast_temperature helper methods

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -11,5 +11,17 @@ describe DarkSkyService, type: :service do
         expect(@_service.details).to be_present
       end
     end
+
+    describe '#forecast_summary' do
+      it 'returns current forecast summary for the latitude longitude' do
+        expect(@_service.forecast_summary).to be_present
+      end
+    end
+
+    describe '#forecast_temperature' do
+      it 'returns current forecast temperature for the latitude longitude' do
+        expect(@_service.forecast_temperature).to be_present
+      end
+    end
   end
 end
