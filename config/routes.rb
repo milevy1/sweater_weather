28b7b87bb435049331f80ec0f732424dd8578378
Matt Levy <milevy1@gmail.com>
2019-06-03 09:41:59 -0600

    Adds antipode route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'backgrounds#show'
+      get '/antipode', to: 'antipode#show'
     end
   end
 end
