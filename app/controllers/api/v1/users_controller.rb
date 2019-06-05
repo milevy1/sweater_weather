1755008b2d1d550af45314d0a105262755711352
Matt Levy <milevy1@gmail.com>
2019-06-05 10:05:15 -0600

    Changes user_controller to user ActionController::API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,4 @@
-class Api::V1::UsersController < ApplicationController
-  protect_from_forgery with: :null_session
-  
+class Api::V1::UsersController < ActionController::API  
   def create
     user = User.new(user_params)
     if user.save
